ea61bd365238aa9ed75f516aaf27562b56c37aaf
ZiQin <ZiQin9510@163.com>
2021-01-30 21:10:44 +0800

    实现页面菜单滑动切换效果 * ProduceForm.kt * PurchaseForm.kt * CommonPagerAdapter.kt

--- app/src/main/java/com/miniwarehouse/ui/form/ProduceForm.kt
+++ app/src/main/java/com/miniwarehouse/ui/form/ProduceForm.kt
@@ -1,12 +1,33 @@
 package com.miniwarehouse.ui.form
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.miniwarehose.R
+import com.miniwarehouse.ui.adapter.CommonPagerAdapter
 
 class ProduceForm : AppCompatActivity() {
+
+    private lateinit var vpager : ViewPager
+    private lateinit var pagerList : ArrayList<View>
+    private lateinit var adapter : PagerAdapter
+
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_produce_form)
+
+        vpager = findViewById(R.id.producePager)
+        val lyinfater : LayoutInflater = layoutInflater
+        pagerList = arrayListOf(
+                lyinfater.inflate(R.layout.pager_item_produce_assembly, null, false),
+                lyinfater.inflate(R.layout.pager_item_produce_product, null, false)
+        )
+        this.adapter = CommonPagerAdapter(pagerList)
+        vpager.adapter = this.adapter
     }
 }
--- app/src/main/java/com/miniwarehouse/ui/form/PurchaseForm.kt
+++ app/src/main/java/com/miniwarehouse/ui/form/PurchaseForm.kt
@@ -1,12 +1,33 @@
 package com.miniwarehouse.ui.form
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.miniwarehose.R
+import com.miniwarehouse.ui.adapter.CommonPagerAdapter
 
 class PurchaseForm : AppCompatActivity() {
+
+    private lateinit var vpager : ViewPager
+    private lateinit var pagerList : ArrayList<View>
+    private lateinit var adapter: PagerAdapter
+
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchase_form)
+
+        vpager = findViewById(R.id.purchasePager)
+        val lyinflater : LayoutInflater = layoutInflater
+        pagerList = arrayListOf(
+                lyinflater.inflate(R.layout.pager_item_purchase_material, null, false),
+                lyinflater.inflate(R.layout.pager_item_purchase_parts, null, false)
+        )
+        this.adapter = CommonPagerAdapter(pagerList)
+        vpager.adapter = this.adapter
     }
 }
