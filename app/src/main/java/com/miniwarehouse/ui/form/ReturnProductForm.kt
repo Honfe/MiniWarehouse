5adad2799b05847df26aece5dc116a14044804f2
ZiQin <ZiQin9510@163.com>
2021-01-30 17:10:11 +0800

    更正小部分命名问题

--- app/src/main/java/com/miniwarehouse/ui/form/ReturnProductForm.kt
+++ app/src/main/java/com/miniwarehouse/ui/form/ReturnProductForm.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.miniwarehose.R
 
-class returnProductForm : AppCompatActivity() {
+class ReturnProductForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_return_product_form)
